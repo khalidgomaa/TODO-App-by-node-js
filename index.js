1204d19f9cb40cbaeb70fb2509eeb924f6ed44a9
khaled <khaled.gomaagedo@gmail.com>
2023-09-01 10:23:53 +0200

    hdfff

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-import express from 'express'
-import connectToDatabase from './db/connection.js'
-import userRouter from './modules/user/user.routes.js'
+import express from 'express';
+import connectToDatabase from './db/connection.js';
+import userRouter from './modules/user/user.routes.js';
+import taskRouter from './modules/task/task.routes.js';
 
-import taskRouter from './modules/task/task.routes.js'
+const app = express();
+app.use(express.json());
+const port = 3000;
+connectToDatabase();
+app.set('view engine', 'ejs');
 
-
-
-const app = express()
-app.use(express.json())
-const port = 3000
-connectToDatabase()
-app.set('view engine','ejs')
 app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-app.get('/', (req,res)=>{
-    res.render('index')
-} )
+app.use(userRouter);
+app.use(taskRouter);
 
-
-
-app.use(userRouter)
-app.use(taskRouter)
-app.listen(port, () => console.log(`toDo app listening on port 3000!`))
+app.listen(port, () => console.log(`toDo app listening on port ${port}!`));
